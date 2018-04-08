43f40985df91640efdc0bfa4a4230ebfd2e10cf3
rkesting <rkesting@grantham.edu>
2018-04-08 10:46:58 -0400

    Add existing file

--- src/pages/favs/favs.ts
+++ src/pages/favs/favs.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ItemSliding } from 'ionic-angular';
 
 @Component({
-  selector: 'favs-home',
+  selector: 'page-favs',
   templateUrl: 'favs.html'
 })
+
 export class FavsPage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+	tasks:Array<any> = [];
+	
+	constructor(public navCtrl: NavController){
+		this.tasks = [
+			{title:'Myakka Pier', status: 'open'},
+			{title:'Sanibel Causeway', status: 'open'},
+			{title:'Matalcha Pier', status: 'open'},
+			{title:'Charlotte Harbor', status: 'open'}
+		];
+	}
+	addItem() {
+		let theNewTask: string = prompt("New Task");
+		if (theNewTask !== '') {
+	this.tasks.push({ title: theNewTask, status: 'open' });
+	}
+	}
+	
+	markAsDone(slidingItem: ItemSliding, task: any) {
+		task.status = "done";
+	}
+	
+	removeTask(slidingItem: ItemSliding,task: any) {
+		task.status = "removed";
+		let index = this.tasks.indexOf(task);
+		if (index > -1) {
+			this.tasks.splice(index, 1);
+		}
+	}	
 }
